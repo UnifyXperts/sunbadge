bb37d784ae45996c8663da830a26d3d3b44d5202
UnifyXperts <support@unifyxperts.com>
2026-05-20 09:01:47 +0530

    add:hide edit opion from list view

---
+++ sunbadge/sunbadge/doctype/traveler/traveler_list.js
@@ -0,0 +1,16 @@
+frappe.listview_settings['Traveler'] = {
+
+    refresh(listview) {
+
+        setTimeout(() => {
+
+            listview.page.actions
+                .find('[data-label="Edit"]')
+                .parent()
+                .remove();
+
+        }, 100);
+
+    }
+
+};
