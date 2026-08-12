05083642d8282dd7dfc8d5df2c5fe7691faaaabc
UnifyXperts <support@unifyxperts.com>
2026-08-12 16:50:43 +0530

    refractor/ hide 'Add to Prospect' from action button from Lead Doctype

--- sunbadge/public/js/lead.js
+++ sunbadge/public/js/lead.js
@@ -1,14 +1,23 @@
 frappe.ui.form.on("Lead", {
-	refresh(frm) {
-		// Hide "Prospect" option from Create menu
-		setTimeout(() => {
-			$("a.dropdown-item").each(function () {
-				let text = $(this).text().trim();
+    refresh(frm) {
+        setTimeout(() => {
+            // Hide Prospect options
+            $("a.dropdown-item").each(function () {
+                const label = $(this).attr("data-label");
 
-				if (text === "Prospect") {
-					$(this).hide();
-				}
-			});
-		}, 300);
-	}
+                if (
+                    label === "Prospect" ||
+                    label === "Add%20to%20Prospect"
+                ) {
+                    $(this).hide();
+                }
+            });
+
+            // Hide Action button
+            $(".page-actions button").filter(function () {
+                return $(this).text().trim().startsWith("Action");
+            }).hide();
+
+        }, 300);
+    }
 });
